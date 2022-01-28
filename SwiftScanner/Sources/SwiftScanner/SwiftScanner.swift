f8846b2f752481564db64260162cefb31e4bbf3f
Petar Filev <petar.filev@wsa.com>
2022-01-28 15:05:18 +0100

    Initial project setup.

---
+++ SwiftScanner/Sources/SwiftScanner/SwiftScanner.swift
@@ -0,0 +1,6 @@
+public struct SwiftScanner {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ SwiftScanner/Tests/SwiftScannerTests/SwiftScannerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftScanner
+
+final class SwiftScannerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftScanner().text, "Hello, World!")
+    }
+}
